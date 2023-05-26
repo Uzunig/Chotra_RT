af73bdb690f8cfed666e284b50278e41ac6d1410
Uzunig <78850854+Uzunig@users.noreply.github.com>
2023-05-26 16:35:26 +0200

    glfw test

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,21 @@
-#include <glm\glm\glm.hpp>
+#include <glm/glm.hpp>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+static bool GLFW_initialized = false;
 
 int main() {
 
     glm::vec3 vector;
 
+    if (!GLFW_initialized) {
+        if (!glfwInit()) {
+            std::cout << "Failed to initialize GLFW" << std::endl;
+            return -1;
+        }
+    }
+    int x;
+    std::cin >> x;
     return 0;
 }
